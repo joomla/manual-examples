36d393ac77194bf208dd5b6c9706c5b24d443206
Robbie Jackson <robbie.jackson@btinternet.com>
2024-05-23 20:25:31 +0100

    step 5 revised

---
+++ module-tutorial/step5_config/mod_hello/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace My\Module\Hello\Site\Dispatcher;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\DispatcherInterface;
+use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Application\CMSApplicationInterface;
+use Joomla\Input\Input;
+use Joomla\Registry\Registry;
+use My\Module\Hello\Site\Helper\HelloHelper;
+
+class Dispatcher implements DispatcherInterface
+{
+    protected $module;
+
+    public function __construct(\stdClass $module, CMSApplicationInterface $app, Input $input)
+    {
+        $this->module = $module;
+    }
+    
+    public function dispatch()
+    {
+        $language = Factory::getApplication()->getLanguage();
+        $language->load('mod_hello');
+        
+        $username = HelloHelper::getLoggedonUsername('Guest');
+
+        $hello = Text::_('MOD_HELLO_GREETING') . $username;
+        
+        $params = new Registry($this->module->params);
+
+        require ModuleHelper::getLayoutPath('mod_hello');
+    }
+}

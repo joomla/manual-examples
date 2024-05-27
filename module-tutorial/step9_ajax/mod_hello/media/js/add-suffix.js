398e541c05ce21b349fc80e7854e4bf591ce1717
Robbie Jackson <robbie.jackson@btinternet.com>
2024-05-27 09:23:45 +0100

    fix vars passed to js

--- module-tutorial/step9_ajax/mod_hello/media/js/add-suffix.js
+++ module-tutorial/step9_ajax/mod_hello/media/js/add-suffix.js
@@ -2,7 +2,7 @@ if (!window.Joomla) {
   throw new Error('Joomla API was not properly initialised');
 }
 
-const { suffix } = Joomla.getOptions('vars');
+const { suffix } = Joomla.getOptions('mod_hello.vars');
 document.querySelectorAll('.mod_hello').forEach(element => {
   element.innerText += suffix;
 });

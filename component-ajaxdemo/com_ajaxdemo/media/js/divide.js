55b0caac9dd882f8983e057cc4d60f85adce722d
Robbie Jackson <robbie.jackson@btinternet.com>
2024-09-17 19:24:05 +0100

    jquery removed plus coding for empty array changed

--- component-ajaxdemo/com_ajaxdemo/media/js/divide.js
+++ component-ajaxdemo/com_ajaxdemo/media/js/divide.js
@@ -1,34 +1,40 @@
 const divide = (event) => {
-    // the element ids get prefixed by whatever is passed as 'control' in the loadForm call in the Model, plus "_"
+    // the element ids are the field names prefixed by whatever is passed as 'control' in the loadForm call in the Model, plus "_"
     let a = document.getElementById("jform_a").value;
     let b = document.getElementById("jform_b").value;
+    let data = { a: a, b: b };
     let answer = document.getElementById("jform_answer");
+    
     // get the URL root which is passed down from the PHP code
-    var vars = Joomla.getOptions('com_ajaxdemo.uri');
-    jQuery.ajax({   
-            url: vars.root + 'index.php?option=com_ajaxdemo&format=json&task=ajax.divide',
-            data: { a: a, b: b },  // passed as HTTP POST parameters
-        })
-        .done(function(result, textStatus, jqXHR)
+    let vars = Joomla.getOptions('com_ajaxdemo.uri');
+    let url = vars.root + 'index.php?option=com_ajaxdemo&format=json&task=ajax.divide';
+
+    fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body:  new URLSearchParams(data).toString()
+    })
+    .then(response => response.json())
+    .then(result => { 
+        if (result.success)
         {
-            if (result.success)
-            {
-                jQuery("#jform_answer").val(result.data); 
-                // render the passed message as an Info message in the messages area
-                Joomla.renderMessages({"info": [result.message]});
-            }
-            else
-            {
-                alert(result.message);
-            }
-            // display the enqueued messages in the message area
-            // pass 3rd param as 'true' so that the previous message doesn't get removed
-            if (result.messages) {
-                Joomla.renderMessages(result.messages, undefined, true);
-            }
-        })
-        .fail(function(jqXHR, textStatus, errorThrown)
+            answer.value = result.data; 
+            // render the passed message as an Info message in the messages area
+            Joomla.renderMessages({"info": [result.message]});
+        }
+        else
         {
-            console.log('ajax call failed' + textStatus);
-        });
+            alert(result.message);
+        }
+        // display the enqueued messages in the message area
+        // pass 3rd param as 'true' so that the previous message doesn't get removed
+        if (result.messages) {
+            Joomla.renderMessages(result.messages, undefined, true);
+        }
+    })
+    .catch( err => {
+        let errName = err.name;
+        let errMsg = err.message;
+        alert(`Ajax failed! error name: ${errName}, message: ${errMsg}`);
+    });
 };

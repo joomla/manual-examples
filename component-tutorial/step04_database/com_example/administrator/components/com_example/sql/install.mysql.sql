e4b43a18b1ef1afc7f2ccbd4c7a78e177bfd9f0d
Robbie Jackson <robbie.jackson@btinternet.com>
2026-06-26 09:32:15 +0100

    renamed table landmark to landmarks

--- component-tutorial/step04_database/com_example/administrator/components/com_example/sql/install.mysql.sql
+++ component-tutorial/step04_database/com_example/administrator/components/com_example/sql/install.mysql.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS `#__landmark`;
+DROP TABLE IF EXISTS `#__landmarks`;
 
-CREATE TABLE `#__landmark` (
+CREATE TABLE `#__landmarks` (
 	`id`        INT(11)     NOT NULL AUTO_INCREMENT,
 	`title`     VARCHAR(25) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-INSERT INTO `#__landmark` (`title`) VALUES
+INSERT INTO `#__landmarks` (`title`) VALUES
 ('The Eiffel Tower'),
 ('The Giant\'s Causeway');
